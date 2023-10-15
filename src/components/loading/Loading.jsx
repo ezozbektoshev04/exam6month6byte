e7550dbd9cce20a5dc462e4ac7b7471a77be7b83
ezozbektoshev04 <toshevezozbek04@gmail.com>
2023-10-15 16:54:06 +0500

    hsdhjsdh

--- src/components/loading/Loading.jsx
+++ src/components/loading/Loading.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ClipLoader from "react-spinners/ClipLoader";
 const override = {
   display: "block",
-  margin: "0 auto",
+  margin: "180px auto",
   borderColor: "red",
 };
 
